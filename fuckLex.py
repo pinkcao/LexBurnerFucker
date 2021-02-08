15fe9f9d4180961218348a3026b05c8e7f3f93e4
pinkcao <972558143@qq.com>
2021-02-08 12:18:35 +0800

    Initial commit

---
+++ fuckLex.py
@@ -0,0 +1,23 @@
+import json,requests,time
+def get():
+    return int(json.loads(requests.get("https://api.bilibili.com/x/relation/stat?vmid=777536").text)["data"]["follower"])
+
+if __name__ == '__main__':
+    fansCountLast = -1
+    filename = 'fuckLex.txt'
+    printFlag = False
+    interval = int(input('请输入获取间隔'))
+
+    while True:
+        file_object = open(filename, 'a')
+        fansCount = get()
+        fansCountMinus = fansCountLast - fansCount
+        localtime = time.asctime(time.localtime(time.time()))
+        lexString = 'lexBurner fans:' + str(fansCount) + ' decreaseNum: ' + str(fansCountMinus) + ' Time: ' + str(localtime)
+        if (printFlag):
+            file_object.write(lexString + '\n')
+            print(lexString)
+        fansCountLast = fansCount
+        printFlag = True
+        file_object.close()
+        time.sleep(interval)
