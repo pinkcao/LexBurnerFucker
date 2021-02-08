b25fefa5c119fefafaed328cc23c490ff727f60e
pinkcao <972558143@qq.com>
2021-02-08 12:37:33 +0800

    update basic usage

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 由下水道居民制作，运行后自行设定以x秒间隔获取lexBurner粉丝数量并保存至同目录txt文件下。
 
 ## basic usage
-clone it and just run
+``` python
+import fuckLex
+fuckLex.getTextAndWrite()
+```
--- fuckLex.py
+++ fuckLex.py
@@ -2,12 +2,11 @@ import json,requests,time
 def get():
     return int(json.loads(requests.get("https://api.bilibili.com/x/relation/stat?vmid=777536").text)["data"]["follower"])
 
-if __name__ == '__main__':
+def getTextAndWrite():
     fansCountLast = -1
     filename = 'fuckLex.txt'
     printFlag = False
     interval = int(input('请输入获取间隔'))
-
     while True:
         file_object = open(filename, 'a')
         fansCount = get()
@@ -21,3 +20,7 @@ if __name__ == '__main__':
         printFlag = True
         file_object.close()
         time.sleep(interval)
+
+
+if __name__ == '__main__':
+    getTextAndWrite()
